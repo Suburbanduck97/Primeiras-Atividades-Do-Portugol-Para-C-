a3165732bb2babae9b2cd141a96204449ff362bf
Ericsson dos Santos <ericssonsantos24@gmail.com>
2024-02-29 19:18:38 -0300

    Add files via upload
    1. Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor
    que C, caso contrário, imprima que a A + B é maior que C.

---
+++ atividade pontuada q1 em.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define texto "Insira os dados abaixo da forma (INTEIRO)."
+#define exibicao "Exibindo Dados Inseridos: "
+
+int main(){
+	//Declarando variáveis
+	int numeroA = 0, numeroB = 0, numeroC = 0 , soma = 0;
+	printf("%s\n", texto);
+	printf("\nINSIRA UM VALOR: ");
+	scanf("%d",&numeroA);
+	
+	printf("INSIRA UM SEGUNDO VALOR: ");
+	scanf("%d",&numeroB);
+	
+	printf("INSIRA UM TERCEIRO VALOR: ");
+	scanf("%d",&numeroC);
+	system("cls");
+	
+	//cálculo do sistema
+	soma = numeroA + numeroB;
+	//Exibindo dados
+	printf("%s\n", exibicao);
+	printf("\nPRIMEIRO NUMERO INSERIDO: %d", numeroA);
+	printf("\nSEGUNDO NUMERO INSERIDO: %d", numeroB);
+	printf("\nTERCEIRO NUMERO INSERIDO: %d", numeroC);
+	printf("\nRESULTADO DA SOMA: %d", soma);
+	
+	//Sistema condicional
+	if(soma > numeroC){
+		printf("\n\nA SOMA DE %d + %d E MAIOR QUE %d", numeroA , numeroB , numeroC);
+	}else if(soma == numeroC){
+		printf("\n\nA SOMA DE %d + %d E IGUAL A %d", numeroA , numeroB , numeroC);
+	}else if(soma < numeroC){
+		printf("\n\nA SOMA DE %d + %d E MENOR QUE %d", numeroA , numeroB , numeroC);		
+	}
+	
+	//Fim do programa
+	return 0;
+	
+}
